6f6546d7f766ba05b0025aa7a44995554b907ef2
aagaev <agaeana@gmail.com>
2024-08-21 17:57:44 +0400

    feat: apply react-router-dom@6

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Row>
-        <div>
-          <Heading>Hellllloooooo</Heading>
-          <Heading as="h1">Hellllloooooo</Heading>
-          <Heading as="h2">Helllllosfafooooo</Heading>
-          <Heading as="h3">Helllllooodfdfdfssooo</Heading>
-        </div>
-      </Row>
-      <div>3ello World</div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+          {/* <Route index element={<Dashboard />} /> */}
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
