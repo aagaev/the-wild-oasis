4269104600b28a9d2e92db8a11c3be9f18930e91
aagaev <agaeana@gmail.com>
2024-08-21 14:35:47 +0400

    install and apply styles components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <Row>
+        <div>
+          <Heading>Hellllloooooo</Heading>
+          <Heading as="h1">Hellllloooooo</Heading>
+          <Heading as="h2">Helllllosfafooooo</Heading>
+          <Heading as="h3">Helllllooodfdfdfssooo</Heading>
+        </div>
+      </Row>
+      <div>3ello World</div>
+    </>
+  );
 }
 
-export default App
+export default App;
