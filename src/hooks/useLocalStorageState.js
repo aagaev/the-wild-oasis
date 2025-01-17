9e42c330955601b64106c01555200b6f6068386d
aagaev <agaeana@gmail.com>
2025-01-17 20:50:12 +0400

    feat: implement dark mode with css variables

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -3,8 +3,14 @@ import { useState, useEffect } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
+    // console.log(storedValue); //false
+    // console.log(JSON.parse(storedValue)); //false
     return storedValue ? JSON.parse(storedValue) : initialState;
   });
+
+  // console.log(initialState);//false
+  // console.log(key); //isDarkMode
+  // console.log(value); //false
 
   useEffect(
     function () {
